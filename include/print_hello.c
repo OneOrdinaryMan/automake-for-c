9081db64df2abb76dcdad09595cbe154faee7d96
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-07-29 16:36:38 +0530

    Added the include directory

---
+++ include/print_hello.c
@@ -0,0 +1,3 @@
+#include "print_hello.h"
+#include <stdio.h>
+void print_hello(void) { puts("Hello, World!"); }
---
+++ include/print_hello.h
@@ -0,0 +1,7 @@
+#ifndef PRINT_HELLO_H
+#define PRINT_HELLO_H
+/*
+ * Prints "Hello, World!\n" to the stdout.
+ */
+void print_hello(void);
+#endif
